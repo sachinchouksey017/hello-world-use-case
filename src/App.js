b583e2649528e752d121b840d3adb1c5f444e89f
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-25 16:32:03 +0530

    Completed with UC-4 for open url onclick of img

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import logo from './assets/img/BridgeLabzNewLogo.svg'
 
 class App extends React.Component {
+  url='http://bridgelabz.com/'
   constructor() {
     super()
     this.state = {
       title: 'hello from Bridgelabz'
     }
   }
+  // onClick function
+  onClick=($event)=>{
+    console.log("save button is clicked!", $event);
+    window.open(this.url,"_blank");
+  }
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.onClick}
         alt="The Bridgelabz logo: a Bridge to Employment through lab works"
 />
       </div>
