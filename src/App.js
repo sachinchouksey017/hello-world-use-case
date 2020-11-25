3902d7eb47d582ea735ceb9826b29cf8362e2195
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-25 16:25:15 +0530

    completed with uc-3 for display logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/img/BridgeLabzNewLogo.svg'
 
 class App extends React.Component {
   constructor() {
@@ -12,6 +13,9 @@ class App extends React.Component {
     return (
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo}
+        alt="The Bridgelabz logo: a Bridge to Employment through lab works"
+/>
       </div>
     );
   }
