e819300963c89f174d93ed573e0c8b774892cdc2
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-25 17:10:54 +0530

    Completed with Uc-8 with react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home/home';
-
+// import react router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class App extends React.Component {
 
   render() {
     return (
       <div className="app-main">
-        <Home />
+        <Router>
+          <Switch>
+            <Route path="">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
