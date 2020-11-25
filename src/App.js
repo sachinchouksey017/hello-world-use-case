050ace6ad9dfaf289f1d14ab2e8c030dcda56cb0
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-25 16:44:31 +0530

    Completed uc-5 to display hello username with user input

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import './App.css';
 import logo from './assets/img/BridgeLabzNewLogo.svg'
 
 class App extends React.Component {
-  url='http://bridgelabz.com/'
+  url = 'http://bridgelabz.com/'
   constructor() {
     super()
     this.state = {
-      title: 'hello from Bridgelabz'
+      userName: ''
     }
   }
   // onClick function
-  onClick=($event)=>{
+  onClick = ($event) => {
     console.log("save button is clicked!", $event);
-    window.open(this.url,"_blank");
+    window.open(this.url, "_blank");
+  }
+  onNameChange = (event) => {
+    console.log("Value is ", event.target.value);
+    this.setState({ userName: event.target.value })
   }
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} onClick={this.onClick}
-        alt="The Bridgelabz logo: a Bridge to Employment through lab works"
-/>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img src={logo} onClick={this.onClick}
+            alt="The Bridgelabz logo: a Bridge to Employment through lab works"
+          />
+        </div>
+        <div className="text-box">
+          <input onChange={this.onNameChange} />
+        </div>
       </div>
     );
   }
